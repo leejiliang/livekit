e6adc08db592963ff862e48fe404416b83e0b1b3
cnderrauber <zengjie9004@gmail.com>
2024-05-23 10:30:44 +0800

    sampling

--- pkg/sfu/buffer/forwardstats.go
+++ pkg/sfu/buffer/forwardstats.go
@@ -2,6 +2,7 @@ package buffer
 
 import (
 	"sync"
+	"sync/atomic"
 	"time"
 )
 
@@ -12,12 +13,12 @@ const (
 )
 
 type ForwardStats struct {
-	lock    sync.Mutex
-	latency uint32
+	lock       sync.Mutex
+	lastLeftMs atomic.Int64
 
-	lastPktTimeStamp time.Time
-	lastTransit      time.Duration
-	jitter           time.Duration
+	// lastPktTimeStamp time.Time
+	lastTransit time.Duration
+	jitter      time.Duration
 
 	delay *LatencyAggregate
 }
@@ -29,19 +30,25 @@ func NewForwardStats() *ForwardStats {
 }
 
 func (s *ForwardStats) Update(arrival, left time.Time) {
+	leftMs := left.UnixMilli()
+	lastMs := s.lastLeftMs.Load()
+	if leftMs < lastMs || !s.lastLeftMs.CompareAndSwap(lastMs, leftMs) {
+		return
+	}
+
 	transit := left.Sub(arrival)
 	s.lock.Lock()
 	defer s.lock.Unlock()
 	s.delay.Update(time.Duration(arrival.UnixNano()), float64(transit))
-	if arrival.Sub(s.lastPktTimeStamp) > jitterUpdateInterval {
-		d := transit - s.lastTransit
-		s.lastTransit = transit
-		if d < 0 {
-			d = -d
-		}
-		s.jitter += (d - s.jitter) / 16
-		s.lastPktTimeStamp = arrival
+	// if arrival.Sub(s.lastPktTimeStamp) > jitterUpdateInterval {
+	d := transit - s.lastTransit
+	s.lastTransit = transit
+	if d < 0 {
+		d = -d
 	}
+	s.jitter += (d - s.jitter) / 16
+	// s.lastPktTimeStamp = arrival
+	// }
 }
 
 func (s *ForwardStats) GetJitter() time.Duration {
